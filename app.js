8b2fa7016c785b88aa88aa899e6cccd2cb35284a
Gosa14488 <gosavolovikov4@yandex.ru>
2025-11-24 01:54:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-// Регистрация сервис-воркера
+// Регистрация Service Worker
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/site-2.0/sw.js");
+    navigator.serviceWorker.register("/site-2.0/sw.js").then(reg => {
 
-    // Слушаем сообщение от SW
+        // Срабатывает, когда найден новый SW
+        reg.onupdatefound = () => {
+            const newWorker = reg.installing;
+
+            newWorker.onstatechange = () => {
+                if (newWorker.state === "installed") {
+
+                    // Если есть активный SW — значит это обновление
+                    if (navigator.serviceWorker.controller) {
+                        showUpdateBanner();
+                    }
+                }
+            };
+        };
+    });
+
+    // Принимаем сообщение от SW
     navigator.serviceWorker.addEventListener("message", event => {
         if (event.data?.type === "NEW_VERSION") {
             showUpdateBanner();
